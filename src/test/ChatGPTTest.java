c37688cfa5531ada21ac5286f177c9041afdcec2
Irene Liao <iireneliao@gmail.com>
2023-05-08 16:12:11 -0700

    test

--- src/test/ChatGPTTest.java
+++ src/test/ChatGPTTest.java
@@ -1,10 +1,10 @@
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ChatGPTTest {
     @Test
