6c8e0c6b77cc542f1fed0c8dd3ef3cd4f16be8cb
Irene Liao <iireneliao@gmail.com>
2023-05-08 17:32:40 -0700

    gradle again

--- test/ChatGPTTest.java
+++ test/ChatGPTTest.java
@@ -1,10 +1,10 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ChatGPTTest {
     @Test
@@ -25,22 +25,6 @@ class ChatGPTTest {
             assertEquals("Hello World", content.trim());
         } catch (IOException e) {
             fail("Unable to read the content of chatGPTResult.txt");
-        }
-    }
-
-    @Test
-    void testWhisper() {
-        Whisper whisper = new Whisper();
-        String[] args = new String[]{"recording.wav"};
-
-        File outputFile = new File("whisperResult.txt");
-        assertTrue(outputFile.exists());
-        
-        try {
-            String content = new String(Files.readAllBytes(Paths.get("whisperResult.txt")));
-            assertEquals("Hello world", content.trim());
-        } catch (IOException e) {
-            fail("Unable to read the content of whisperResult.txt");
         }
     }
 }
