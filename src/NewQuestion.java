a5cb4c1fd281486c13735dae24e7785db73fefc8
Irene Liao <iireneliao@gmail.com>
2023-05-10 19:08:04 -0700

    modify newquestion

--- src/NewQuestion.java
+++ src/NewQuestion.java
@@ -10,58 +10,18 @@ public class NewQuestion {
         audioRecorder.startRecording();
     }
 
-    public void newQuestionEnd() throws JSONException, IOException, InterruptedException {
+    public void newQuestionStop() throws JSONException, IOException, InterruptedException {
         audioRecorder.stopRecording();
 
         String whisperArg = "myAudio.mp3";
         String question = whisper.getTranscript(whisperArg);
 
         chatGPT.chat(question);
-        // String[] argsForWhisper = {"myAudio.mp3"}; //output file of audioRecorder
-
-        // try {
-        //     Class<?> class1 = Class.forName("Whisper");
-        //     Method method = class1.getMethod("main", String[].class);
-        //     String[] arguments = argsForWhisper;
-        //     method.invoke(null, (Object) arguments);
-        // } catch (ClassNotFoundException e) {
-        //     e.printStackTrace();
-        // } catch (NoSuchMethodException e) {
-        //     e.printStackTrace();
-        // } catch (SecurityException e) {
-        //     e.printStackTrace();
-        // } catch (IllegalAccessException e) {
-        //     e.printStackTrace();
-        // } catch (IllegalArgumentException e) {
-        //     e.printStackTrace();
-        // } catch (InvocationTargetException e) {
-        //     e.printStackTrace();
-        // }
-
-        // String[] argsForChatGPT = {"100", "whisperResult.txt"}; //transcript output file of Whisper
-        // try {
-        //     Class<?> class1 = Class.forName("ChatGPT");
-        //     Method method = class1.getMethod("main", String[].class);
-        //     String[] arguments = argsForChatGPT;
-        //     method.invoke(null, (Object) arguments);
-        // } catch (ClassNotFoundException e) {
-        //     e.printStackTrace();
-        // } catch (NoSuchMethodException e) {
-        //     e.printStackTrace();
-        // } catch (SecurityException e) {
-        //     e.printStackTrace();
-        // } catch (IllegalAccessException e) {
-        //     e.printStackTrace();
-        // } catch (IllegalArgumentException e) {
-        //     e.printStackTrace();
-        // } catch (InvocationTargetException e) {
-        //     e.printStackTrace();
-        // }
     }
     public static void main(String[] args) throws JSONException, IOException, InterruptedException {
         NewQuestion q = new NewQuestion();
         q.newQuestionStart();
-        q.newQuestionEnd();
+        q.newQuestionStop();
     }
     
 }
