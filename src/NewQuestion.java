4add976923e024791ce24fb3157a48a87311fcc3
Irene Liao <iireneliao@gmail.com>
2023-05-12 10:40:23 -0700

    Refactor NewQuestion.java

--- src/NewQuestion.java
+++ src/NewQuestion.java
@@ -5,29 +5,35 @@ public class NewQuestion {
     private static AudioRecorder audioRecorder = new AudioRecorder();
     private static Whisper whisper = new Whisper();
     private static ChatGPT chatGPT = new ChatGPT();
+    private boolean isRecording = true;
 
-    public void newQuestionStart(){
+    public void start(){
         audioRecorder.startRecording();
     }
 
-    public void newQuestionStop() throws JSONException, IOException, InterruptedException {
+    public void stop() throws JSONException, IOException, InterruptedException {
         audioRecorder.stopRecording();
     }
-    public static void main(String[] args) throws JSONException, IOException, InterruptedException {
-        NewQuestion q = new NewQuestion();
-        q.newQuestionStart();
 
-        boolean isRecording = true;
-
+    public void recordingStatus() throws InterruptedException{
         while (isRecording) {
             isRecording = audioRecorder.isRecording();
-            Thread.sleep(100);
+            Thread.sleep(25);
         }
+    }
 
-        q.newQuestionStop();
+    public void display() throws JSONException, IOException, InterruptedException {
         String whisperArg = "myAudio.mp3";
         String question = whisper.getTranscript(whisperArg);
         chatGPT.chat(question); 
+    }
+
+    public static void main(String[] args) throws JSONException, IOException, InterruptedException {
+        NewQuestion q = new NewQuestion();
+        q.start();
+        q.recordingStatus();
+        q.display();
+        q.stop();
     }
     
 }
