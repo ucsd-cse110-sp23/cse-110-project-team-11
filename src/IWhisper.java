4a6e5e0f5c9b5df6b29870793c5282aa07a46035
Peike Xu <p3xu@ucsd.edu>
2023-05-19 16:49:54 -0700

    whisper mocking

---
+++ src/IWhisper.java
@@ -0,0 +1,7 @@
+public class IWhisper extends Whisper{
+
+    @Override
+    public String getTranscript(String filePath){
+        return "This is a test transcript";
+    }
+}
--- test/WhisperTest.java
+++ test/WhisperTest.java
@@ -1,26 +1,13 @@
 import org.junit.jupiter.api.Test;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class WhisperTest {
+class WisperTest {
+    Whisper whisper = new IWhisper();
 
     @Test
-    void testWhisper() {
-        Whisper whisper = new Whisper();
-        String[] args = new String[]{"recording.mp3"};
-
-        File outputFile = new File("whisperResult.txt");
-        assertTrue(outputFile.exists());
-        
-        try {
-            String content = new String(Files.readAllBytes(Paths.get("whisperResult.txt")));
-            assertEquals("Hello world!", content.trim());
-        } catch (IOException e) {
-            fail("Unable to read the content of whisperResult.txt");
-        }
+    void raisetest() throws IOException {
+        String transcript = whisper.getTranscript(null);
+        assertEquals("This is a test transcript",transcript);
     }
 }
