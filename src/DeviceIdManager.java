92c6b135ebd2c95525cbf7860f59792f4fbc130d
railgun5 <88987127+railgun5@users.noreply.github.com>
2023-05-31 16:58:00 -0700

    Return the device ID

---
+++ src/DeviceIdManager.java
@@ -0,0 +1,22 @@
+import java.util.prefs.Preferences;
+
+public class DeviceIdManager {
+    private static final String DEVICE_ID_KEY = "device_id";
+    private Preferences preferences;
+
+    public DeviceIdManager() {
+        preferences = Preferences.userNodeForPackage(DeviceIdManager.class);
+    }
+
+    public String getDeviceId() {
+        String deviceId = preferences.get(DEVICE_ID_KEY, null);
+        if(deviceId == null) {
+            deviceId = UUID.randomUUID().toString();
+            preferences.put(DEVICE_ID_KEY, deviceId);
+        }
+        return deviceId;
+    }
+}
+
+
+
