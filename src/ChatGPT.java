694abd8b3e5ab274d67ac03db0a495acd565ad5b
Rena Hu <88347346+Rena2025@users.noreply.github.com>
2023-06-05 18:11:16 -0700

    Update ChatGPT.java

--- src/ChatGPT.java
+++ src/ChatGPT.java
@@ -11,7 +11,6 @@ import org.json.JSONObject;
 class ChatGPT{
     private static final String API_ENDPOINT = "https://api.openai.com/v1/completions";
     private static final String API_KEY = "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";
-    private static final String API_KEY = "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";
     private static final String MODEL = "text-davinci-003";
     
     private String question;
