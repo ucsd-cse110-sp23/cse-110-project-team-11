91a1303340b71ecd4ff7b1ada2c9a70a120ffe38
Rena Hu <88347346+Rena2025@users.noreply.github.com>
2023-06-05 18:16:02 -0700

    Commented out BDDTest.java

--- test/BDDTest.java
+++ test/BDDTest.java
@@ -1,52 +1,52 @@
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import java.io.IOException;
-import java.util.ArrayList;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.json.JSONException;
+// import org.json.JSONObject;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+// import java.io.IOException;
+// import java.util.ArrayList;
+// import static org.junit.jupiter.api.Assertions.*;
 
-public class BDDTest {
-    private VoiceCommandsMock vcm;
-    private ArrayList<JSONObject> hpm;
-    private JsonStorage js;
+// public class BDDTest {
+//     private VoiceCommandsMock vcm;
+//     private ArrayList<JSONObject> hpm;
+//     private JsonStorage js;
 
-    @BeforeEach
-    public void setup() throws IOException {
-        vcm = new VoiceCommandsMock();
-        hpm = new ArrayList<JSONObject>();
-        js = new JsonStorage("historyPromptMock.json");
-    }
+//     @BeforeEach
+//     public void setup() throws IOException {
+//         vcm = new VoiceCommandsMock();
+//         hpm = new ArrayList<JSONObject>();
+//         js = new JsonStorage("historyPromptMock.json");
+//     }
 
-    @Test
-    /**
-     * Tests BDD Scenario 3 from US4
-     */
-    public void testUS4() throws IOException {
-        ///////////////////////////////////////////////////////
-        //  Manually create historyprompt list
-        //ArrayList<JSONObject> jsonArr = new ArrayList<>();
+//     @Test
+//     /**
+//      * Tests BDD Scenario 3 from US4
+//      */
+//     public void testUS4() throws IOException {
+//         ///////////////////////////////////////////////////////
+//         //  Manually create historyprompt list
+//         //ArrayList<JSONObject> jsonArr = new ArrayList<>();
 
-        JSONObject jo = new JSONObject();
-        jo.put("question", "answer");
-        hpm.add(jo);
+//         JSONObject jo = new JSONObject();
+//         jo.put("question", "answer");
+//         hpm.add(jo);
 
-        js.setHistoryPrompt(hpm);
+//         js.setHistoryPrompt(hpm);
 
-        js.writeJson("historyPromptMock.json");
-        //
+//         js.writeJson("historyPromptMock.json");
+//         //
 
-        ///////////////////////////////////////////////////////
+//         ///////////////////////////////////////////////////////
 
-        vcm.clearAll();
+//         vcm.clearAll();
 
-        assertEquals(1, hpm.size());
+//         assertEquals(1, hpm.size());
         
-        /**
-         * create a new historyprompt list
-         * * add JSONObject 
-         * call clear all from voice commands (mock version)
-         * check to see that list size == 1
-         */
-    }
-}
+//         /**
+//          * create a new historyprompt list
+//          * * add JSONObject 
+//          * call clear all from voice commands (mock version)
+//          * check to see that list size == 1
+//          */
+//     }
+// }
